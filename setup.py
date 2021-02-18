4c65f26e36c378b7376d3ade87c97195eb9ec2a2
bobbintb <bobbintb@gmail.com>
2021-02-18 14:02:24 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='utorrentctl',
+    version='',
+    packages=['utorrent'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
