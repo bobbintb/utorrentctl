d7a4f8b9a54e845ba809013073c29e343522b0a6
bobbintb <bobbintb@gmail.com>
2021-02-18 18:14:40 -0700

    initial commit, added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='utorrentctl',
+    version='1.0',
+    packages=['utorrent'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
